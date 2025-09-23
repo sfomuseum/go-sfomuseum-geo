57681d5bd683b6b3aa739103e7e705fd2839c8c9
thisisaaronland <thisisaaronland@localhost>
2025-09-22 17:44:37 -0700

    uncomment georef constant

--- constants.go
+++ constants.go
@@ -6,7 +6,7 @@ const RESERVED_GEOTAG_BELONGSTO string = "geotag:whosonfirst_belongsto"
 
 const RESERVED_GEOREFERENCE_BELONGSTO string = "georef:whosonfirst_belongsto"
 
-// const RESERVED_GEOREFERENCE_DEPICTIONS string = "georef:depictions"
+const RESERVED_GEOREFERENCE_DEPICTIONS string = "georef:depictions"
 
 const RESERVED_GEOREFERENCE_LABEL string = "georef:label"
 
